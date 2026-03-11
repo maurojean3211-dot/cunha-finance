d8a9d620b65ba4717bf57800c0edab9a83a27925
Mauro Cunha <seuemail@gmail.com>
2026-03-11 15:34:57 -0300

    create pix api

--- src/api/pix.js
+++ src/api/pix.js
@@ -1,26 +1,18 @@
 export default async function handler(req, res) {
 
-// ===== LIBERAR CORS =====
-
 res.setHeader("Access-Control-Allow-Origin", "*");
 res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
 res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-// ===== RESPOSTA PARA PREFLIGHT =====
-
 if (req.method === "OPTIONS") {
 return res.status(200).end();
 }
-
-// ===== BLOQUEAR OUTROS MÉTODOS =====
 
 if (req.method !== "POST") {
 return res.status(405).json({ erro: "Método não permitido" });
 }
 
 try {
-
-// ===== CHAMADA PARA API ASAAS =====
 
 const response = await fetch("https://api.asaas.com/v3/payments", {
 method: "POST",
@@ -31,19 +23,7 @@ headers: {
 body: JSON.stringify(req.body)
 });
 
-// ===== PEGAR RESPOSTA =====
-
-const text = await response.text();
-
-if (!text) {
-return res.status(500).json({
-erro: "Resposta vazia do Asaas"
-});
-}
-
-const data = JSON.parse(text);
-
-// ===== RETORNAR PIX =====
+const data = await response.json();
 
 return res.status(200).json(data);
 
